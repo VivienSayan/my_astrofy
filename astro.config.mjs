3be38142b0fdac8283cb36578863bed5bf83f8d2
VivienSayan <pravong.vivien@gmail.com>
2026-01-28 15:38:40 +0100

    Force static output for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  output: 'static',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
