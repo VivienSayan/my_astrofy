63090b484d11a59dd5fafe886d271ec84475cca6
VivienSayan <pravong.vivien@gmail.com>
2026-01-28 15:41:33 +0100

    Force static output for netlify - remoded URL from file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
   output: 'static',
   integrations: [mdx(), sitemap(), tailwind()],
 });
